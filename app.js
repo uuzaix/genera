1e485015721a522d98627ea87b5657bdd5120b63
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-02-24 23:50:59 +0100

    Add superMemo2 function and tests

--- app.js
+++ app.js
@@ -2,5 +2,39 @@ export function updateEntry(entry, answer) {
     return entry;
 }
 
-export function superMemo2() {
+export function superMemo2(entry, answer) {
+	// if (answer.quality < 2 || answer.quality > 5) {
+	// 	throw "Quality should be between 2 and 5";
+	// }
+
+	//calculate repetition and EF based on answer.quality. 
+	if(answer.quality == 2){
+		entry.repetition = 1 //proceed as a new word without updating the EF
+	}
+	else {
+		entry.EF = entry.EF + (0.1 - (5-answer.quality)*(0.08+(5-answer.quality)*0.02)) //calculate new EF
+		entry.repetition += 1
+	}
+
+
+	if(entry.repetition == 1) {
+		entry.interval = 1
+	}
+	else if(entry.repetition == 2){
+		entry.interval = 6
+	}
+	else {
+		entry.interval = Math.round(entry.interval*entry.EF)
+	}
+
+	return entry
 }
+
+// var entry = {id: 10, word: 'rue', genus: 'F', interval: 1, repetition: 1, EF: 1.0 } 
+// var answer =  {answer: 'F', sure: true, quality: 2}  // interval: 1, repetition: 1, EF: 2.0 
+// var answer1 =  {answer: 'F', sure: true, quality: 4} //interval: 6, repetition: 6, EF: 2.0 
+// var answer2 =  {answer: 'F', sure: true, quality: 0} //interval: 12, repetition: 6, EF: 2.0 
+
+// console.log(superMemo2(entry, answer))
+// console.log(superMemo2(entry, answer1))
+// console.log(superMemo2(entry, answer2))
--- app.test.js
+++ app.test.js
@@ -7,3 +7,26 @@ describe('updateEntry', function() {
         expect(e.id).to.equal(10);
     });
 }); 
+
+describe('superMemo2', function() {
+    it('should update interval and repetition to 1 when quality is 2', function() {
+        const quality2 = superMemo2({interval: 5, repetition: 5, EF: 2.0 }, {quality: 2});
+        expect(quality2.interval).to.equal(1);
+        expect(quality2.repetition).to.equal(1);
+        expect(quality2.EF).to.equal(2.0);
+    });
+    it('should calculate correct values', function() {
+        const quality4 = superMemo2({interval: 5, repetition: 5, EF: 2.0 }, {quality: 4});
+        expect(quality4.interval).to.equal(10);
+        expect(quality4.repetition).to.equal(6);
+        expect(quality4.EF).to.equal(2.0);
+        const quality5 = superMemo2({interval: 1, repetition: 1, EF: 1.0 }, {quality: 5});
+        expect(quality5.interval).to.equal(6);
+        expect(quality5.repetition).to.equal(2);
+        expect(quality5.EF).to.equal(1.1);
+    });
+    // it('should check if quality is between 2 and 5', function() {
+    //     const quality0 = superMemo2({interval: 5, repetition: 5, EF: 2.0 }, {quality: 1});
+    //     assert.throws(quality0, Error, "Quality should be between 2 and 5");
+    // });
+}); 
