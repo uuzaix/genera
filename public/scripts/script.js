f25fbf539e042b773abb7f1afecdb90fe0754abf
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-04-29 22:28:40 +0200

    Update client

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,24 @@
 
 var DictonaryBox = React.createClass({
 
+  handleInputSubmit: function(answ) {
+    var answer = this.state.data;
+    this.setState({data: answer});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: answ,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data: comments});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   getInitialState: function() {
     return {data: []};
   },
@@ -24,7 +42,7 @@ var DictonaryBox = React.createClass({
       <div className="dictonaryBox">
         <h1>GENERA</h1>
         <Word data={this.state.data} />
-        <UserInput/>
+        <UserInput onInputSubmit={this.handleInputSubmit} />
       </div>
     );
   }
@@ -41,15 +59,47 @@ var Word = React.createClass({
 });
 
 var UserInput = React.createClass({
+  getInitialState: function() {
+    return {id: '', genus: '', sure: false};
+  },
+  handleGenusChange: function(e) {
+    this.setState({genus: e.target.value});
+  },
+  handleSureChange: function(e) {
+    this.setState({sure: !this.state.sure});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var genus = this.state.genus.trim();
+    var sure = this.state.sure;
+    if (!genus || !sure) {
+      return;
+    }
+    this.props.onInputSubmit({genus: genus, sure: sure});
+    this.setState({genus: '', sure: false});
+  },
+
   render: function() {
     return (
-      <form>
-        <input type="text" placeholder="Genus..." />
+      <form className="userInput" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Genus..."
+          value={this.state.genus}
+          onChange={this.handleGenusChange}
+        />
+
         <p>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            sure={this.props.sure}
+            ref="sure"
+            onChange={this.handleSureChange}
+          />
           {' '}
           Sure?
         </p>
+        <input type="submit" value="Post" />
       </form>
     );
   }
